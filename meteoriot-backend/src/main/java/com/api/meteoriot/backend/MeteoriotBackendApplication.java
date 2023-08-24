9292a4807e9f7029d61d74734683411b2d27b0d7
Vinícius Basílio <vbasilio2019@gmail.com>
2023-08-24 19:51:27 -0300

    feature: add basic front-end and back-end

---
+++ meteoriot-backend/src/main/java/com/api/meteoriot/backend/MeteoriotBackendApplication.java
@@ -0,0 +1,13 @@
+package com.api.meteoriot.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeteoriotBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeteoriotBackendApplication.class, args);
+	}
+
+}
---
+++ meteoriot-backend/src/test/java/com/api/meteoriot/backend/MeteoriotBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.meteoriot.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeteoriotBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
