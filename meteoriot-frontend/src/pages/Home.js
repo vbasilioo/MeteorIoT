28be4ad7ddb198a9a12f379e2c0802d08c245b6b
Vinícius Basílio <vbasilio2019@gmail.com>
2023-08-28 20:06:40 -0300

    feature: instalação do TailwindCSS.

--- meteoriot-frontend/src/pages/Home.js
+++ meteoriot-frontend/src/pages/Home.js
@@ -2,7 +2,9 @@ import React from "react";
 
 const Home = () =>{
     return(
-        <h1>Home</h1>
+        <h1 class="text-3xl font-bold underline">
+            Hello world!
+        </h1>
     );
 };
 
