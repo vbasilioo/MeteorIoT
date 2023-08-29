3f1344afea9109a23eba35f42be399cc808723fc
Vinícius Basílio <vbasilio2019@gmail.com>
2023-08-29 20:28:41 -0300

    feature: arquivos alterados de JS para JSX (padrão React).

--- meteoriot-frontend/src/App.jsx
+++ meteoriot-frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
 import Home from "./pages/Home";
+import Dashboard from './pages/Dashboard/Dashboard';
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path='/dashboard' element={<Dashboard />} />
       </Routes>
     </Router>
   );
---
+++ meteoriot-frontend/src/pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Sidebar from "../../components/Sidebar";
+
+export default function Dashboard(){
+    return(
+        <Sidebar />
+    );
+}
