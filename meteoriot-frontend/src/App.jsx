6c19b722c176640c7e12979b21aebdda518dc469
Vinícius Basílio <vbasilio2019@gmail.com>
2023-09-12 21:38:26 -0300

    feature: add sistema de temperatura aleatório.

--- meteoriot-frontend/src/App.jsx
+++ meteoriot-frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from "./pages/Home";
 import Dashboard from './pages/Dashboard/Dashboard';
 import Login from './pages/Login/Login';
+import CadastrarUsuario from './pages/Login/CadastrarUsuario';
 
 function App() {
 
